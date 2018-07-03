4ab1d6df273ee23824190291cdfe5d9da8537c15
Jieqiong Yu <jieyu@linkedin.com>
2018-07-03 18:58:30 +0800

    learning notes for javascript essential training

---
+++ JavaScript Essential Training/4 Functions and Objects/Closure/script.js
@@ -0,0 +1,15 @@
+function doSomeMath() {
+    var a = 5;
+    var b = 4;
+
+    function multiply() {
+        var result = a * b;
+        return result;
+    }
+
+    return multiply;
+}
+
+var theResult = doSomeMath();
+
+// console.log("The result: ", theResult());
