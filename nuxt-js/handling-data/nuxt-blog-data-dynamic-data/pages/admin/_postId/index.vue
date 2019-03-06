7ef5e6aba019f46011ca65ff582f9ea32ca706ac
Jieqiong Yu <jieqiong-yu@mercari.com>
2019-03-06 19:22:20 +0900

    Add demo demonstrating about changing static postlist to dynamic data - /nuxt-js/handling-data/nuxt-blog-dynamic-data

---
+++ nuxt-js/handling-data/nuxt-blog-data-dynamic-data/pages/admin/_postId/index.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="admin-post-page">
+    <section class="update-form">
+      <AdminPostForm :post="loadedPost"/>
+    </section>
+  </div>
+</template>
+
+<script>
+  import AdminPostForm from '@/components/Admin/AdminPostForm'
+
+  export default {
+    layout: 'admin',
+    components: {
+      AdminPostForm
+    },
+    data() {
+      return {
+        loadedPost:{
+          author: 'Mer',
+          title: 'My first Post',
+          content: 'Super amazing, thanks for that!!',
+          thumbnailLink: 'http://cryptotimes.org/wp-content/uploads/2018/06/050318_LRR_MEN_WomenTech.jpg'
+        }
+
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .update-form {
+    width: 90%;
+    margin: 20px auto;
+  }
+
+  @media (min-width: 768px) {
+    .update-form {
+      width: 500px;
+    }
+  }
+</style>
---
+++ nuxt-js/handling-data/nuxt-blog-data-dynamic-data/pages/posts/index.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="posts-page">
+      <PostList />
+    </div>
+</template>
+
+<style scoped>
+.posts-page {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+</style>
+
+<script>
+  import PostList from "@/components/Posts/PostList";
+  export default {
+    components: {PostList}
+  }
+</script>
